18854fb848b80944bfafef980cc7e1372529fd24
Elena <marsik-123@mail.ru>
2023-02-08 08:55:33 +0300

    n

---
+++ task_n.py
@@ -0,0 +1,18 @@
+# t()
+
+# print(type(t))
+
+# t =(1, 3, 9)
+# print(type(t))
+
+v = [1, 3, 9]
+print(v)
+print(type(v))
+
+v = tuple(v)
+print(v)
+print(type(v))
+
+a,b = 1, 2
+a,b,c = v
+print(a,b,c)
