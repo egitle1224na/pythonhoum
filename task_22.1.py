80ad3cda376b21ca12b9e628ba407d202febf029
Elena <marsik-123@mail.ru>
2023-02-08 10:11:03 +0300

    дополнение

--- task_22.1.py
+++ task_22.1.py
@@ -40,3 +40,28 @@ def SortRevList(m, n):
 #     kool.sort()
 # for i in kool:
 #     print(i, end=' ')
+
+# a = {2, 4, 6, 8, 10, 12, 10, 8, 6, 4, 2}
+# b = {3, 6, 9, 12, 15, 18}
+def SortRevList(a, b):
+    a = set(a)
+    b = set(b)
+    c = a.intersection(b)
+    c = list(c)
+    c.sort(reverse=False)
+    return c
+
+
+asize = int(input('Введите размер множества а: '))
+bsize = int(input('Введите размер множества b: '))
+a = []
+b = []
+print('Введите элементы первого списка')
+for _ in range(asize):
+    a.append(input())
+print('Введите элементы второго списка')
+for _ in range(bsize):
+    b.append(input())
+print(a)
+print(b)
+print(SortRevList(a, b))
